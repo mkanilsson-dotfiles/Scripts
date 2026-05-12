26751707defe8bd3b5879120eb77d2a7e1fa1fd7
Marcus Nilsson <marcus.nilsson@genarp.com>
2026-05-12 17:21:31 +0200

    Update start_nvim

--- start_nvim.sh
+++ start_nvim.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 export ZVM_INSTALL="$HOME/.zvm/self"
 export PATH="$PATH:$HOME/.zvm/bin"
 export PATH="$PATH:$ZVM_INSTALL/"
+export PATH="$PATH:$ZVM_INSTALL/"
+export PATH="$PATH:$HOME/.cargo/bin"
 
 nvim
