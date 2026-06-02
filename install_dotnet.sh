2df9e0d1878d53a6f6660e3667e68a58c4937be0
Marcus Nilsson <marcus.nilsson@genarp.com>
2026-06-02 11:23:22 +0200

    Fetch dotnet version via aur

--- install_dotnet.sh
+++ install_dotnet.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-sudo pacman -S "dotnet-runtime-$1" "aspnet-runtime-$1" "aspnet-targeting-pack-$1" "dotnet-sdk-$1"
+paru -S "dotnet-runtime-$1" "aspnet-runtime-$1" "aspnet-targeting-pack-$1" "dotnet-sdk-$1"
